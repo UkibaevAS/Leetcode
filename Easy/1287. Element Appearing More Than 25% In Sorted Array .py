84555e1a27ecf5059352d42803f4e20a45164354
UkibaevAS <Ukibaev_AS@mail.ru>
2023-12-13 11:29:17 +0500

    done

--- Easy/1287. Element Appearing More Than 25% In Sorted Array .py
+++ Easy/1287. Element Appearing More Than 25% In Sorted Array .py
@@ -22,3 +22,11 @@ class Solution:
             dct.setdefault(i, 0)
             dct[i] += 1
         return max(dct, key=dct.get)
+
+class Solution:
+    def findSpecialInteger(self, arr: List[int]) -> int:
+        n = len(arr)
+        quarter = n // 4
+        for i in range(n - quarter):
+            if arr[i] == arr[i + quarter]:
+                return arr[i]
